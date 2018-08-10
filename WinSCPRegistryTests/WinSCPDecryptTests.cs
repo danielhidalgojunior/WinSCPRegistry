5700e4c195eebf677574921c9deaa7962b9a2dfa
Daniel Hidalgo <danielhidalgo@cooperative.com.br>
2018-08-10 13:29:06 -0300

    decryption test unit

---
+++ WinSCPRegistryTests/WinSCPDecryptTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WinSCP_Reg_Decrypt;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinSCP_Reg_Decrypt.Tests
+{
+    [TestClass()]
+    public class WinSCPDecryptTests
+    {
+        [TestMethod()]
+        public void DecryptTest()
+        {
+            string host = "testing.com";
+            string username = "testing";
+            string password = "A35C47501B70C17D87CBD1D1984C28E828392F2835323B28392F2835323B723F33316D28392F2835323B6E42BFBA5CA4ABAE";
+
+            var w = new WinSCPDecrypt();
+            string result = w.Decrypt(host, username, password);
+
+            if (result != "1testing2")
+                Assert.Fail();
+        }
+    }
+}
